e79b4ed8188dc5abd4efd3acb19f3492b4e32227
mat <mateuscat55@hotmail.com>
2025-02-28 16:09:59 -0300

    feat: adding endpoint to register an user

---
+++ src/Backend/MyRecipeBookAPI/Controllers/UserController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using MyRecipeBook.Communication.Requests;
+using MyRecipeBook.Communication.Responses;
+
+namespace MyRecipeBookAPI.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        [HttpPost]
+        [ProducesResponseType(typeof(ResponseRegisteredUserJson), StatusCodes.Status201Created)]
+        public IActionResult Registrer(RequestRegisterUserJson request)
+        {
+            return Created();
+        }
+    }
+}
