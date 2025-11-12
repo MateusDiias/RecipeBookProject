cac1d8b68194044e8db65c62bd4463fec49f46ef
mat <mateuscat55@hotmail.com>
2025-11-12 18:06:56 -0300

    fix coding smell

--- src/Backend/MyRecipeBook.Infraestructure/Security/Tokens/Access/JwtTokenHandler.cs
+++ src/Backend/MyRecipeBook.Infraestructure/Security/Tokens/Access/JwtTokenHandler.cs
@@ -9,7 +9,7 @@ namespace MyRecipeBook.Infrastructure.Security.Tokens.Access
 {
     public abstract class JwtTokenHandler
     {
-        protected SymmetricSecurityKey SecurityKey(string signinKey)
+        protected static SymmetricSecurityKey SecurityKey(string signinKey)
         {
             var bytes = Encoding.UTF8.GetBytes(signinKey);
 
